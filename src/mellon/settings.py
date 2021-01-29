cdc092f1fae5de8311121629665ed71c8e45330c
SergioSenem <sergio.e.senem@gmail.com>
2021-01-28 21:43:25 -0300

    Primeiro migration

--- src/mellon/settings.py
+++ src/mellon/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mellon_app.apps.MellonAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- src/mellon_app/models.py
+++ src/mellon_app/models.py
@@ -3,4 +3,14 @@ from django.db import models
 
 class Event(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField
+    name = models.CharField(max_length=200)
+    message = models.CharField(max_length=200)
+    date = models.DateTimeField
+    has_reminder = models.BooleanField
+    day_of_week = models.IntegerField
+
+class Reminder(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    message = models.CharField(max_length=200)
+    date = models.DateTimeField
+    sent = models.BooleanField(default=False)
